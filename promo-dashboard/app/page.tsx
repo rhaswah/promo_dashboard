cc1473a9d7638e0584c5d7d0ccd06efbbcf63a8b
rhaswah <44286471+rhaswah@users.noreply.github.com>
2025-04-21 17:01:00 -0500

    dashboard code

---
+++ promo-dashboard/app/page.tsx
@@ -0,0 +1,9 @@
+import PromoDashboard from "./components/promo_dashboard";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-white p-6">
+      <PromoDashboard />
+    </main>
+  );
+}
